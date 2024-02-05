f094e64da07ee78bfe5d8db8f7802148cca503d5
dbmadden <158228848+dbmadden@users.noreply.github.com>
2024-02-05 14:34:12 -0500

    Script.R

---
+++ Script.R
@@ -0,0 +1,9 @@
+library(msa)
+setwd("/Users/iamdeanooo/Documents/GitHub/Bioinformatics")
+setwd"/Users/iamdeanooo/Documents/GitHub/Bioinformatics"
+x<- read.csv(data.csv)
+x
+x <- 1
+x
+attach(mtcars)
+getwd
