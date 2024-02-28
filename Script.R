1336081a201c881950d9b795888b73ee27fd146e
henicorhina <ojohnson@utep.edu>
2024-02-28 08:06:03 -0500

    update midterm.R

--- Script.R
+++ Script.R
@@ -1,6 +1,6 @@
 library(msa)
 setwd("/Users/iamdeanooo/Documents/GitHub/Bioinformatics")
-setwd"/Users/iamdeanooo/Documents/GitHub/Bioinformatics")
+setwd("/Users/iamdeanooo/Documents/GitHub/Bioinformatics")
 x<- read.csv(data.csv)
 x
 x <- 1
